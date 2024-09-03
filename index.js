b63b154115ad40e9e864cd7301fb9c7325ed9c8e
LucasAlfajor <167381353+LucasAlfajor@users.noreply.github.com>
2024-09-03 23:02:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Lucas";
+const Sobrenome = "Lourenço";
+const Idade = 16;
+const CidadeNatal = "Curitiba";
+const Altura = 1.71;
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+console.log("Meu sobrenome é:")
+console.log(Sobrenome);
+console.log("Minha idade é:")
+console.log(Idade);
+console.log("Nasci na cidade de:")
+console.log(CidadeNatal);
+console.log("Minha altura é:")
+console.log(Altura);
